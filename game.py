7e2835b1fa1b77c6d8ea998545ac1813e24b73a9
ivan <ibodnar@outlook.com>
2019-05-11 17:28:37 -0300

    game.py added -> LogicalBoard class - LogicalBoard.fill_square

---
+++ game.py
@@ -0,0 +1,20 @@
+from exceptions import FilledSquareException
+
+
+class LogicalBoard:
+    def __init__(self):
+        self._board = {x: None for x in range(10)}
+        self._occupied_squares = []
+
+    def fill_square(self, square_number, value):
+        if not self._board.get(square_number):
+            self._board[square_number] = value
+        else:
+            raise FilledSquareException('Square is Occupied')
+
+    def _show_board(self):
+        print(self._board)
+
+
+
+
