b40f34f09cd0b3b540082e13d21cdc40ca5b3eae
ivan <ibodnar@outlook.com>
2019-05-11 22:51:24 -0300

    First implamentation of the main loop

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-from game import LogicalBoard
+from board import LogicalBoard, PhysicalBoard, WINNING_COMBINATIONS
+from validators import validate_user_input
+from exceptions import MoveNotInRangeException
 
 
 b = LogicalBoard()
-b.fill_square(1, 1)
+# b.fill_square(1, 'x')
+p = PhysicalBoard(b)
+p.draw()
+p.register_move(1, 'x')
+
+
+while True:
+    try:
+        move = validate_user_input(input('Enter move'))
+    except ValueError:
+        print('Must enter an integer')
+        continue
+    except MoveNotInRangeException:
+        print('Must enter a value between 0 and 8')
+        continue
 
