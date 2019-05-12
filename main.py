5de33a7ac1221527c8248bddb1d0eefc84bfe70f
ivan <ibodnar@outlook.com>
2019-05-11 23:08:07 -0300

    Added register_move in main loop

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from exceptions import MoveNotInRangeException
 
 
 b = LogicalBoard()
-# b.fill_square(1, 'x')
 p = PhysicalBoard(b)
 p.draw()
-p.register_move(1, 'x')
 
 
 while True:
     try:
-        move = validate_user_input(input('Enter move'))
+        move = validate_user_input(input('Enter move\n'))
     except ValueError:
         print('Must enter an integer')
         continue
     except MoveNotInRangeException:
         print('Must enter a value between 0 and 8')
         continue
+
+    p.register_move(move, 'x')
 
