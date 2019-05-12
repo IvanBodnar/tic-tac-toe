c62240a80f26044c03a32b779ef5e8daa12a35a2
ivan <ibodnar@outlook.com>
2019-05-11 21:34:12 -0300

    PhysicalBoard class added

--- game.py
+++ game.py
@@ -37,3 +37,14 @@ class LogicalBoard:
 
     def __str__(self):
         return str(self._board)
+
+
+class PhysicalBoard:
+    def __init__(self, logical_board: LogicalBoard):
+        self._logical_board = logical_board
+
+    def draw(self):
+        board = self._logical_board.state
+        print('|', board.get(0) or 0, '|', board.get(1) or 1, '|', board.get(2) or 2, '|')
+        print('|', board.get(3) or 3, '|', board.get(4) or 4, '|', board.get(5) or 5, '|')
+        print('|', board.get(6) or 6, '|', board.get(7) or 7, '|', board.get(8) or 8, '|')
